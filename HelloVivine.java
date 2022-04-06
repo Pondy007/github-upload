7065250e22b98d3d48130d481434e6da1fdff8eb
Vivine Assokane <assokane@gmail.com>
2022-04-06 23:57:17 +0200

    initial commit - moving project

---
+++ HelloVivine.java
@@ -0,0 +1,11 @@
+public class HelloVivine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Vivine!");
+		System.out.println("Ceci est ma 2nde ligne.");
+		System.out.println("Ceci est ma 3e ligne.");
+		System.out.println("J'aime le café et le chocolat");
+	}
+
+}
